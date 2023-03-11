23514c5d4a004e9cc228815739b8241d9fa54a4e
Sammoned <artem.korovnikov@mail.ru>
2023-03-11 21:32:57 +0300

    Залил заглушки для фильтров, сделал анимации для них же

--- client/src/components/FilterList/FilterList2.js
+++ client/src/components/FilterList/FilterList2.js
@@ -4,7 +4,16 @@ import classes from './FilterList2.module.css'
 const FilterList2 = ({active}) => {
     return (
         <div className={active ? classes.filterBlock + " " + classes.active : classes.filterBlock}>
-            
+            <ul>
+                <li>Фильтр 1</li>
+                <li>Фильтр 2</li>
+                <li>Фильтр 3</li>
+                <li>Фильтр 4</li>
+                <li>Фильтр 5</li>
+                <li>Фильтр 6</li>
+                <li>Фильтр 7</li>
+                <li>Фильтр 8</li>
+            </ul>
         </div>
     );
 };
